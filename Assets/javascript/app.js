9bf012412e1d4aa2b9f4b50053eeca542108fe4c
Ted Hirsch <tdhirsch9@gmail.com>
2020-03-01 23:31:53 -0700

    mostly finished html and css

---
+++ Assets/javascript/app.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var change = document.getElementById("startButton");
+    if (change.style.display === "none") {
+      change.style.display = "block";
+    } else {
+      change.style.display = "none";
+    }
+  }
